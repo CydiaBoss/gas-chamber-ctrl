c8d47899e9c8ba560b6a9f9aee8a554349b7ea60
Andrew Wang <andrew_wang2002@hotmail.com>
2024-06-24 11:57:28 -0600

    more work on plot

--- constants.py
+++ constants.py
@@ -1,1 +1,12 @@
+from pglive.kwargs import Axis
+
+# General
 APPID = "medal.gaschamber.uc"
+
+# Axis
+TIME_AXIS_CONFIG = {
+    "orientation": "bottom",
+    "units": "s",
+    "tick_angle": -45,
+    Axis.TICK_FORMAT: Axis.DURATION,
+}
--- main_wrapper.py
+++ main_wrapper.py
@@ -1,6 +1,14 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QCoreApplication
+
+from pglive.sources.live_axis import LiveAxis
 
 from main_gui import Ui_MainWindow
+
+from constants import *
+
+# Translating Function
+_t = lambda x="" : QCoreApplication.translate("MainWindow", x)
 
 class Window(Ui_MainWindow, QMainWindow):
 
@@ -8,3 +16,42 @@ class Window(Ui_MainWindow, QMainWindow):
         super().__init__(None)
 
         self.setupUi(self)
+
+        self.setup_plots()
+
+    def setup_plots(self):
+        """
+        Setup the plots
+        """
+        # Setup Background
+        self.temp_plot.setBackground("w")
+        self.humidity_plot.setBackground("w")
+        self.pressure_plot.setBackground("w")
+        self.resistance_plot.setBackground("w")
+
+        # Setup Axis Items
+        self.temp_plot.getPlotItem().setAxisItems({
+            "left": LiveAxis(orientation="left", text=_t("Temperature"), units="degC"),
+            "bottom": LiveAxis(text=_t("Time"), **TIME_AXIS_CONFIG)
+        })
+
+        self.humidity_plot.getPlotItem().setAxisItems({
+            "left": LiveAxis(orientation="left", text=_t("Humidity"), units="%RH"),
+            "bottom": LiveAxis(text=_t("Time"), **TIME_AXIS_CONFIG)
+        })
+
+        self.pressure_plot.getPlotItem().setAxisItems({
+            "left": LiveAxis(orientation="left", text=_t("Pressure"), units="psi"),
+            "bottom": LiveAxis(text=_t("Time"), **TIME_AXIS_CONFIG)
+        })
+
+        self.resistance_plot.getPlotItem().setAxisItems({
+            "left": LiveAxis(orientation="left", text=_t("Resistance"), units="Ohm"),
+            "bottom": LiveAxis(text=_t("Time"), **TIME_AXIS_CONFIG)
+        })
+
+        # Setup Labels
+        self.temp_plot.getPlotItem().setLabels(title=_t("Temperature"), left=_t("Temperature"), bottom=_t("Time"))
+        self.humidity_plot.getPlotItem().setLabels(title=_t("Humidity"), left=_t("Humidity"), bottom=_t("Time"))
+        self.pressure_plot.getPlotItem().setLabels(title=_t("Pressure"), left=_t("Pressure"), bottom=_t("Time"))
+        self.resistance_plot.getPlotItem().setLabels(title=_t("Resistance"), left=_t("Resistance"), bottom=_t("Time"))
